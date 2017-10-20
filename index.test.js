735292a9308f51d969c3bee8bbb28696f39caf87
Zachary R. Smith <ZacharySmith4989@gmail.com>
2017-10-20 12:23:35 -0400

    chore: Make test ES5

--- index.test.js
+++ index.test.js
@@ -3,66 +3,76 @@ var mongoose = require('mongoose');
 var transformProps = require('.');
 
 var SubDocSchema = new mongoose.Schema({
-	foo: String
+  foo: String
 });
 
 var ModelSchema = new mongoose.Schema({
-	subDoc: SubDocSchema
+  subDoc: SubDocSchema
 });
 
 var Model = mongoose.model('Model', ModelSchema);
 
-describe('transformProps', () => {
-	var castToString = prop => String(prop);
-	let doc;
+describe('transformProps', function() {
+  var castToString = function(prop) {
+    return String(prop);
+  };
+  var doc;
 
-	beforeEach(() => {
-		doc = new Model({
-			subDoc: { foo: 'bar' }
-		}).toObject();
-	});
+  beforeEach(function() {
+    doc = new Model({
+      subDoc: {foo: 'bar'}
+    }).toObject();
+  });
 
-	it('throws a TypeError when passed a bad @object', () => {
-		var badArgsSet = new Set([
-			['notAnObject', castToString, '_id'],
-			[doc, 'notAFunction', '_id'],
-			[doc, castToString, () => 'bad @propKeys']
-		]);
+  it('throws a TypeError when passed a bad @object', function() {
+    var badArgsSet = new Set([
+      ['notAnObject', castToString, '_id'],
+      [doc, 'notAFunction', '_id'],
+      [doc, castToString, function() {
+        return 'bad @propKeys';
+      }]
+    ]);
 
-		badArgsSet.forEach(badArgs => {
-			// "Note: You must wrap the code in a function, otherwise the error will not be caught and the assertion will fail."
-			// https://facebook.github.io/jest/docs/en/expect.html#tothrowerror
-			var act = () => transformProps(...badArgs);
+    badArgsSet.forEach(function(badArgs) {
+      // "Note: You must wrap the code in a function, otherwise the error will not be caught and the assertion will fail."
+      // https://facebook.github.io/jest/docs/en/expect.html#tothrowerror
+      var act = function() {
+        transformProps.apply(null, badArgs);
+      };
 
-			expect(act).toThrow(TypeError);
-		});
-	});
+      expect(act).toThrow(TypeError);
+    });
+  });
 
-	it('can convert `_id`s to string', () => {
-		['_id', ['_id']].forEach(propKeysArg => { // Test that @propKeys of type str and single-item array are ok.
-			transformProps(doc, castToString, propKeysArg);
+  it('can convert `_id`s to string', function() {
+    ['_id', ['_id']].forEach(function(propKeysArg) { // Test that @propKeys of type str and single-item array are ok.
+      transformProps(doc, castToString, propKeysArg);
 
-			expect(typeof doc._id).toBe('string');
-			expect(typeof doc.subDoc._id).toBe('string');
-		});
-	});
+      expect(typeof doc._id).toBe('string');
+      expect(typeof doc.subDoc._id).toBe('string');
+    });
+  });
 
-	it('can use multiple transformers', () => {
-		var appendHello = str => str + 'Hello!';
+  it('can use multiple transformers', function() {
+    var appendHello = function(str) {
+      return str + 'Hello!';
+    };
 
-		transformProps(doc, [castToString, appendHello], '_id');
+    transformProps(doc, [castToString, appendHello], '_id');
 
-		expect(doc._id.substr(-6)).toBe('Hello!');
-		expect(doc.subDoc._id.substr(-6)).toBe('Hello!');
-	});
+    expect(doc._id.substr(-6)).toBe('Hello!');
+    expect(doc.subDoc._id.substr(-6)).toBe('Hello!');
+  });
 
-	it('can transform multiple props', () => {
-		var appendHello = str => str + 'Hello!';
+  it('can transform multiple props', function() {
+    var appendHello = function(str) {
+      return str + 'Hello!';
+    };
 
-		transformProps(doc, [castToString, appendHello], ['_id', 'foo']);
+    transformProps(doc, [castToString, appendHello], ['_id', 'foo']);
 
-		expect(doc._id.substr(-6)).toBe('Hello!');
-		expect(doc.subDoc._id.substr(-6)).toBe('Hello!');
-		expect(doc.subDoc.foo).toBe('barHello!');
-	});
+    expect(doc._id.substr(-6)).toBe('Hello!');
+    expect(doc.subDoc._id.substr(-6)).toBe('Hello!');
+    expect(doc.subDoc.foo).toBe('barHello!');
+  });
 });
